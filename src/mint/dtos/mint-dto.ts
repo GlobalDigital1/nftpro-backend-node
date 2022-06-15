d814c96cc01edc1699facbb2b78595d5bd54f8c4
ostappp02 <Ostap02pylyp06>
2022-06-15 16:55:44 +0300

    validation

--- src/mint/dtos/mint-dto.ts
+++ src/mint/dtos/mint-dto.ts
@@ -1,9 +1,15 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class MintDto {
+  @IsNotEmpty()
   accountId: string;
 
+  @IsNotEmpty()
   tokenId: string;
 
+  @IsNotEmpty()
   name: string;
 
+  @IsNotEmpty()
   description: string;
 }
